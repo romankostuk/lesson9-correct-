49e3922aec5ebabbf4b90b39caa0b519bdd8ecbc
romankostuk <moryak.man@list.ru>
2024-06-10 21:04:56 +0300

    Add files via upload

---
+++ Lesson9.py
@@ -0,0 +1,8 @@
+first = int(input('Введите целое число: '))
+second = int(input('Введите целое число: '))
+third = int(input('Введите целое число: '))
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else: print(0)
